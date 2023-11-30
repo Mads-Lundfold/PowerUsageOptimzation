c8001b8cf5d350176a107f67a0aae932efabe51a
Mads-Lundfold <lundfold.business@gmail.com>
2023-11-30 13:01:57 +0100

    Removed data_grapper, finished major refactoring.

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import numpy as np
 import datetime
 import sys
 import re
-from data_grapper import DataManager
 
 from events import get_events
 from price_data import get_price_data
@@ -12,168 +11,47 @@ from time_associations import find_time_associations
 from patterns import mine_temporal_patterns, find_patterns_on_day
 from optimization import optimize
 
+def main():
+    start = 1388617200
+    end = 1420066799
 
-start = 1388617200
-end = 1420066799
+    usage = read_usage_data_csv()
+    #print(usage)
 
-usage = read_usage_data_csv()
-#print(usage)
+    events = get_events(usage)
+    #print(events)
 
-events = get_events(usage)
-#print(events)
+    time_associations = find_time_associations(usage, threshold=0.1)
+    #print(time_associations)
 
-time_associations = find_time_associations(usage, threshold=0.2)
-#print(time_associations)
+    mine_temporal_patterns(support=0.1, confidence=0.6)
 
-mine_temporal_patterns(support=0.1, confidence=0.6)
+    day_range = pd.date_range(start='01/05/2014', end='12/28/2014')
 
-day_range = pd.date_range(start='01/05/2014', end='12/28/2014')
-
-num_infeasible = 0
-
-for day in day_range:
-    try:
-        next_day = (day + datetime.timedelta(days=1))
-        print(day, next_day)
-
-        events_on_day = events[events['start'].between(day, next_day)]
-
-        print(events_on_day)
-
-        patterns_on_day = find_patterns_on_day('output/Experiment_minsup0.1_minconf_0.6/level2.json', events_on_day)
-
-
-        # Quantizing data
-        # FORMAT PRICES
-        day_price = day.replace(year = 2015)
-        next_day_price = next_day.replace(year = 2015, hour = 23)
-        #print(day_price, next_day_price)
-        prices = get_price_data(day_price, next_day_price)
-        price_vector = np.array(np.repeat(prices['GB_GBN_price_day_ahead'], 4))
-        #print(price_vector)
-
-        # FORMAT EVENTS
-        events_on_day['start'] = events_on_day['start'].apply(lambda x: x.hour*4 + (x.minute // 15))
-        events_on_day['duration'] = events_on_day['duration'].apply(lambda x: x // 15)
-
-        print(events_on_day)
-
-        #events['profile'] = events['profile'].apply(lambda x: [float(idx) for idx in x.strip("[]").split(', ')])
-
-        #print(price_vector)
-        #print(events)
-
-        optimize(events=events_on_day, prices=price_vector, patterns=patterns_on_day, time_associations=time_associations)
-
-    except Exception as e:
-        pass
-
-print(num_infeasible)
-
-sys.exit()
-
-#TODO Comments on code
-#TODO Restructure entire project with nice architecture
-#TODO Make frontend :O
-
-
-TESTING_OPT = True
-TESTING_ONE_DAY = not True
-
-
-dm = DataManager()
-dm.get_usage_data(start, end)
-print("Successfully loaded the data.")
-running = True
-
-
-if TESTING_ONE_DAY:
-    day = datetime.datetime(year=2014, month=1, day=17)
-    next_day = day + datetime.timedelta(days=1)
-    try:
-        dm.optimize(day, next_day)
-    except:
-        print('whoopsie')
-
-    print(f'Cost before {dm.cost_before}')
-    print(f'Cost after  {dm.cost_after}')
-    
-
-if TESTING_OPT:
-    #dm.load_temporal_patterns('output/Experiment_minsup0.1_minconf_0.6/level2.json')
-    total_cost = 0
-    exception_days = list()
-    day_range = pd.date_range(start='01/04/2014', end='12/28/2014')
-    for i in day_range:
+    for day in day_range:
         try:
-            next_day = (i + datetime.timedelta(days=1))
+            next_day = (day + datetime.timedelta(days=1))
+
+            events_on_day = events[events['start'].between(day, next_day)]
+
+            patterns_on_day = find_patterns_on_day('output/Experiment_minsup0.1_minconf_0.6/level2.json', events_on_day)
+
+            # Quantizing data
+            # FORMAT PRICES
+            day_price = day.replace(year = 2015)
+            next_day_price = next_day.replace(year = 2015, hour = 23)
             
-            dm.optimize(i, next_day)
+            prices = get_price_data(day_price, next_day_price)
+            price_vector = np.array(np.repeat(prices['GB_GBN_price_day_ahead'], 4))
+            
+            # FORMAT EVENTS
+            events_on_day['start'] = events_on_day['start'].apply(lambda x: x.hour*4 + (x.minute // 15))
+            events_on_day['duration'] = events_on_day['duration'].apply(lambda x: x // 15)
+
+            optimize(events=events_on_day, prices=price_vector, patterns=patterns_on_day, time_associations=time_associations)
 
         except Exception as e:
-            exception_days.append({'day': i, 'msg': e})
             pass
-    
-    print(f'Cost before {dm.cost_before}')
-    print(f'Cost after  {dm.cost_after}')
-    
-    for e in exception_days:
-        print(e['day'], e['msg'])
-    
-    #dm.check_events_without_starting_times()
-elif False:
-    while(running):
-        print("Enter a command:")
-        command = input(">> ")
 
-        match command:
-            case 'load':
-                dm.get_usage_data(start, end)
-
-                print("Successfully loaded the data.")
-            case 'mine':
-                support = 0.10
-                confidence = 0.60
-                dm.mine_temporal_patterns(support, confidence)
-            case 'o':
-                dm.optimize()
-            case 'q':
-                running = False
-            case '_':
-                print('Command Not recognized.')
-
-
-#data = dm.get_usage_data(start, end)
-
-#dm.get_price_data(1,2)
-'''
-def main():
-    print('Finding events.')
-    events = get_events_from_data(start, end)
-    print('Found events:')
-    print(events)
-    
-    price_data = get_price_data(start, end)
-    print(price_data)
-
-    return
-    find_time_associations()
-    find_temporal_patterns()
-
-    problem = create_optimization_problem()
-    problem.solve()
-
-
-
-
-def find_time_associations():
-    return True
-
-def find_temporal_patterns():
-    return True
-
-def create_optimization_problem():
-    return 
 
 main()
-'''
